113c208eda0a5be796a0eb7dd26399f50d30c009
jony-007 <habiburrahmanjony4@gmail.com>
2021-10-12 00:32:47 +0600

    fixed the spacing problem of recipe list with grid and added more recipe item

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-$(".recipes").click(function () {
-  window.location = $(".myBox").data("/recipes/biriyani.html");
+// $(".recipes").click(function () {
+//   window.location = $(".myBox").data("/recipes/biriyani.html");
 
-  return false;
-});
+//   return false;
+// });
 
-console.log("pikachu");
+// console.log("pikachu");
+var links = document.getElementsByTagName("a");
+var len = links.length;
+
+for (var i = 0; i < len; i++) {
+  links[i].target = "_blank";
+}
